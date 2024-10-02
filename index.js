a6c9ef98a573708d2fc1890a9ea97671c0f2c9de
AnakinGit69 <anupamplays2005@gmail.com>
2024-10-02 23:25:18 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["working for anakin 👴🏻"];
 
 
 let currentIndex = 0;
